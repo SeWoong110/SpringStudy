41ba0f247793b944b965792d986bae33b9d19be9
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-11-05 18:59:49 +0900

    [9. API] API 방식 이용

--- src/main/java/springboot/springboot/controller/HelloController.java
+++ src/main/java/springboot/springboot/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -29,5 +30,41 @@ public class HelloController {
         model.addAttribute("name", name);
 
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    // @ResponsBody는 html 응답 바디에 리턴값을 바로 넣어주겠다는 애너테이션임
+    // 이 전처럼 html파일이 아닌 순수 문자열이 전달됨
+    // 이렇게 문자만 전달하는 방식은 거의 안씀
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name") String name) {
+        return "hello " + name; // "hello string"
+    }
+
+    @GetMapping("hello-api")
+    // viewResolver가 아니라 HttpMessageConverter가 동작
+    // 여기서 문자인지 객체인지에 따라 StringConverter or JsonConverter가 동작함
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name") String name) {
+
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        // 객체 자체를 리턴
+        // json 형식의 파일이 리턴됨
+        return hello;
+    }
+
+    // getter/setter
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
